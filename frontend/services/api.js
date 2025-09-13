155f12df248cfba8617ab43641f7b1dbda11d32a
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-14 06:45:41 +0800

    Create api.js

---
+++ frontend/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useAuthStore } from '../stores/auth';
+
+const apiClient = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to add the auth token to headers
+apiClient.interceptors.request.use(
+  (config) => {
+    const authStore = useAuthStore();
+    const token = authStore.accessToken;
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
